04b58b397a6424e3ac0d27e7cdca0c66f17924d7
saibhavya2000 <62001864+saibhavya2000@users.noreply.github.com>
2026-02-28 23:06:29 +0530

    Add fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fib(n):
+    a, b = 0, 1
+    result = []
+    for i in range(n):
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+if __name__ == "__main__":
+    print(fib(10))
